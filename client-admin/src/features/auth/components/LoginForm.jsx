ea600263ba7f817b3aa1f89b03167957c4935df2
elopez-2021393 <elopez-2021393@kinal.edu.gt>
2026-04-23 08:49:37 -0600

    Arreglar confirmModal

--- client-admin/src/features/auth/components/LoginForm.jsx
+++ client-admin/src/features/auth/components/LoginForm.jsx
@@ -1,96 +1,28 @@
-import { useForm } from "react-hook-form"
-import { useAuthStore } from "../store/authStore.js";
-import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import React from "react";
 
-export const LoginForm = ({ onForgot }) => {
-    const navigate = useNavigate();
-    const login = useAuthStore((state) => state.login);
-    const loading = useAuthStore((state) => state.loading);
-    const error = useAuthStore((state) => state.error);
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
-
-    const onSubmit = async (data) => {
-        const res = await login(data);
-        if (res.success) {
-            navigate('/dashboard');
-            toast.success("¡Bienvenido a KinalSports!", { duration: 3000 });
-        }
-    }
-
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-            <div>
-                <label
-                    htmlFor="emailOrUsername"
-                    className="block text-sm font-medium text-gray-800 mb-1.5"
-                >
-                    Email o Username
-                </label>
-                <input
-                    type="text"
-                    id="emailOrUsername"
-                    placeholder="correo@example.com o Username"
-                    className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-                    {
-                    ...register("emailOrUsername", {
-                        required: "Este campo es obligatorio"
-                    })
-                    }
-                />
-                {errors.emailOrUsername && (
-                    <p className="text-red-600 text-xs mt-1">
-                        {errors.emailOrUsername.message}
-                    </p>
-                )}
-            </div>
-
-            <div>
-                <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-800 mb-1.5"
-                >
-                    Constraseña
-                </label>
-                <input
-                    type="password"
-                    id="password"
-                    placeholder="* * * * * * *"
-                    className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-                    {
-                    ...register("password", {
-                        required: "Este campo es obligatorio"
-                    })
-                    }
-                />
-
-                {errors.password && (
-                    <p className="text-red-600 text-xs mt-1">
-                        {errors.password.message}
-                    </p>
-                )}
-            </div>
-            {error && <p className="text-red-600 text-sm text-center">{error}</p>}
-            <button
-                type="submit"
-                disabled={loading}
-                className="w-full bg-main-blue hover:opacity-90 text-white font-medium py-2.5 px-4 rounded-lg transition-colors duration-200 text-sm"
-            >
-                {loading ? "Iniciando..." : "Iniciar Sesión"}
-            </button>
-            <p className="text-center text-sm">
+export function showConfirmToast({ title, message, onConfirm }) {
+    toast.custom((t) => (
+        <div className="bg-white p-6 rounded-xl w-96 text-center shadow-lg border border-gray-200">
+            <h2 className="text-xl font-bold mb-2">{title}</h2>
+            <p className="mb-4">{message}</p>
+            <div className="flex justify-center gap-4 mt-4">
                 <button
-                    type="button"
-                    onClick={onForgot}
-                    className='text-main-blue hover:underline hover:cursor-pointer'
+                    onClick={() => toast.dismiss(t.id)}
+                    className="px-5 py-2 rounded-lg bg-gray-200 text-gray-700 font-medium hover:bg-gray-300 transition"
                 >
-                    ¿Olvidaste tu contraseña?
+                    Cancelar
                 </button>
-            </p>
-        </form>
-    )
+                <button
+                    onClick={() => {
+                        onConfirm?.();
+                        toast.dismiss(t.id);
+                    }}
+                    className="px-5 py-2 rounded-lg bg-red-600 text-white font-medium hover:bg-red-700 transition"
+                >
+                    Confirmar
+                </button>
+            </div>
+        </div>
+    ));
 }
